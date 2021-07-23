b3ed2bd7e5070e89157600fac00c239e2ebe2e19
Alexa Lagunas <amlagunas@college.harvard.edu>
2021-07-23 01:09:33 +0000

    sensor data

--- web.py
+++ web.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm
 from flask_behind_proxy import FlaskBehindProxy
 from flask_sqlalchemy import SQLAlchemy
+import time, random, threading
+from turbo_flask import Turbo
+from audio import printWAV
+
 
 
 app = Flask(__name__)
@@ -10,6 +14,10 @@ app.config['SECRET_KEY'] = 'a8125438efaec1f814c546b434b591e8'
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+
+interval=10
+FILE_NAME = "DeadPoetsSociety.wav"
+turbo = Turbo(app)
 
 class User(db.Model):
   id = db.Column(db.Integer, primary_key=True)
@@ -44,6 +52,47 @@ def register():
       
       
     return render_template('register.html', title='Register', form=form)
+  
+@app.route("/captions")
+def captions():
+    TITLE = "DeadPoetsSociety"
+#     FILE_NAME = "DeadPoetsSociety.wav"
+    return render_template('captions.html', songName=TITLE, file=FILE_NAME)
+
+@app.before_first_request
+def before_first_request():
+    #resetting time stamp file to 0
+    file = open("pos.txt","w") 
+    file.write(str(0))
+    file.close()
+
+    #starting thread that will time updates
+    threading.Thread(target=update_captions, daemon=True).start()
+
+@app.context_processor
+def inject_load():
+    # getting previous time stamp
+    file = open("pos.txt","r")
+    pos = int(file.read())
+    file.close()
+
+    # writing next time stamp
+    file = open("pos.txt","w")
+    file.write(str(pos+interval))
+    file.close()
+
+    #returning captions
+    return {'caption':printWAV(FILE_NAME, pos=pos, clip=interval)}
+
+def update_captions():
+    with app.app_context():
+        while True:
+            # timing thread waiting for the interval
+            time.sleep(interval)
+
+            # forcefully updating captionsPane with caption
+            turbo.push(turbo.replace(render_template('captionsPane.html'), 'load'))
+            
   
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
